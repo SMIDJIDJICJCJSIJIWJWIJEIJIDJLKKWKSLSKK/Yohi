0ac94eaf41659353965aee1f47b24f5ab556d88d
Spectatxr <wavtheprime@gmail.com>
2026-01-31 22:05:46 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -42,7 +42,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/SMIDJIDJICJCJSIJIWJWIJEIJIDJLKKWKSLSKK/Yohi/'..readfile('ReVape/profiles/commit.txt')..'/'..select(1, path:gsub('ReVape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/Spectatxr/Yohi/'..readfile('ReVape/profiles/commit.txt')..'/'..select(1, path:gsub('ReVape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -92,7 +92,7 @@ end
 
 if not shared.VapeDeveloper then
 	local _, subbed = pcall(function() 
-		return game:HttpGet('https://github.com/SMIDJIDJICJCJSIJIWJWIJEIJIDJLKKWKSLSKK/Yohi') 
+		return game:HttpGet('https://github.com/Spectatxr/Yohi') 
 	end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
